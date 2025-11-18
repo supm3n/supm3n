329a149df82ab1fcb8dfa19e90d08a9ff2c7a323
supm3n <supm3n@gmail.com>
2025-11-18 20:54:07 +0100

    Change build format to file to fix 404s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    // This creates 'projects.html' instead of 'projects/index.html'
+    // It fixes 404 issues on some Cloudflare configurations
+    format: 'file'
+  }
+});
