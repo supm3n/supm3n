30561696042ba01db414ffc62037aebfc16f979f
supm3n <supm3n@gmail.com>
2025-11-20 00:41:21 +0100

    Added functions to Entropy Project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: 'https://supm3n.com',
   build: {
     // Removed "format: 'file'" to prevent redirect conflicts
-  }
+  },
+
+  integrations: [sitemap()]
 });
