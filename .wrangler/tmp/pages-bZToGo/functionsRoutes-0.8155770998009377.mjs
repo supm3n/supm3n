bc94b3438f28eb22606c40ade105fbe4b90c887c
supm3n <supm3n@gmail.com>
2025-11-20 22:23:48 +0100

    Improved Charts

---
+++ .wrangler/tmp/pages-bZToGo/functionsRoutes-0.8155770998009377.mjs
@@ -0,0 +1,27 @@
+import { onRequest as __api_cron_update_js_onRequest } from "E:\\Supm3n\\functions\\api\\cron\\update.js"
+import { onRequest as __api_financials__ticker__js_onRequest } from "E:\\Supm3n\\functions\\api\\financials\\[ticker].js"
+import { onRequest as __api_price__symbol__js_onRequest } from "E:\\Supm3n\\functions\\api\\price\\[symbol].js"
+
+export const routes = [
+    {
+      routePath: "/api/cron/update",
+      mountPath: "/api/cron",
+      method: "",
+      middlewares: [],
+      modules: [__api_cron_update_js_onRequest],
+    },
+  {
+      routePath: "/api/financials/:ticker",
+      mountPath: "/api/financials",
+      method: "",
+      middlewares: [],
+      modules: [__api_financials__ticker__js_onRequest],
+    },
+  {
+      routePath: "/api/price/:symbol",
+      mountPath: "/api/price",
+      method: "",
+      middlewares: [],
+      modules: [__api_price__symbol__js_onRequest],
+    },
+  ]
